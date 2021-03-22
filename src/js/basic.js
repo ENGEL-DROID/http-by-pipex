5ab54a5a235de4c8811fb9e2cd4ab195d49beba7
Engel Restrepo <restrepoengelberth@gmail.com>
2021-03-22 18:35:00 +0100

    Memes

--- src/js/basic.js
+++ src/js/basic.js
@@ -11,9 +11,9 @@ export const cuerpo = () => {
     <div id="contenido">
         <h2>Los proyectos creados son los siguientes:</h2>
         <ul>
-            <li><a id="chiste" href="">Chistes con Api 😂</a></li>
-            <li><a id="crud" href="">CRUD de usuarios 👨‍👩‍👦</a></li>
-            <li><a id="upload" href="">Subida y lectura de imágenes 🖼️</a></li>
+            <li><a id="chiste" href="">😂 Chistes con Api</a></li>
+            <li><a id="crud" href="">👨‍👩‍👦 CRUD de usuarios</a></li>
+            <li><a id="upload" href="">🖼️ Subida y lectura de imágenes</a></li>
         </ul>
     </div>
     <div id="carga">
@@ -47,7 +47,7 @@ const escuchar = () => {
 }
 
 const montarChiste = () => {
-    let cont = 0;
+    let cont = 1;
     const div = document.getElementById('carga');
     div.innerHTML = '';
     const contenido = `
@@ -55,19 +55,25 @@ const montarChiste = () => {
         <h2>CHISTES:</h2>
         <hr>
         <a href="">
-        <button>Generar un chiste</button>
+        <button id="floatBtn">Nuevo<br>Chiste</button>
         </a>
         </div>
     `;
     div.innerHTML = contenido;
     const btn = document.querySelector('button');
     btn.addEventListener('click', async (e) => {
+        const random = Math.floor(Math.random() * (99 - 0) + 0);
+        console.log("Número Random: " + random);
         e.preventDefault();
-        const chiste = await (await getChiste()).joke;
+        // const chiste = await (await getChiste()).joke;
+        const chiste = await (await getChiste()).data.memes[random].name;
+        const urlImg = await (await getChiste()).data.memes[random].url;
+        // console.log((await getChiste()).data.memes);
         const divInt = document.createElement('div');
         const elemento = `
         <hr>
-        <p> ${cont++} - ${chiste} </p>
+        <p> ${cont++} - ${chiste}:</p>
+        <img src="${urlImg}" alt="img">
         `;
         divInt.innerHTML = elemento;
         div.append(divInt);
--- src/js/http-providers.js
+++ src/js/http-providers.js
@@ -1,11 +1,14 @@
-const urlChistes = 'https://v2.jokeapi.dev/joke/Any?type=single';
+// const urlChistes = 'https://v2.jokeapi.dev/joke/Any?type=single';
+const urlChistes = 'https://api.imgflip.com/get_memes';
 
 const getChiste= async () => {
     try {
         const obj = await fetch(urlChistes);
         if (!obj.ok) throw 'Algo salió mal con la url de la Api';
-        const {joke} = await obj.json();
-        return {joke};
+        // const {joke} = await obj.json();
+        // return {joke};
+        const {success, data} = await obj.json();
+        return {success, data};
     } catch (err) {
         throw err;
     }
